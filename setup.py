69f8761063dc9b73b90e2b27d602762ac5825a7a
Salvador Fandiño <sfandino@yahoo.com>
2021-05-01 00:50:54 +0200

    Add long description in installer

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@
 
 from setuptools import setup
 
+import os
+pkg_dir = os.path.abspath(os.path.dirname(__file__))
+readme_fn = os.path.join(pkg_dir, 'README.md')
+with open(readme_fn, encoding='utf-8') as file:
+    long_description = file.read()
+
 setup(
     name='fastdbfs',
     version='0.2',
     description="Interactive command line client for Databricks DBFS",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="http://github.com/salva/fastdbfs",
     author="Salvador Fandiño García",
     author_email="sfandino@yahoo.com",
